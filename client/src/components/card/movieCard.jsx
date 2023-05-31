cb85063375422bec6d735316f6ae58dc4f83325b
mayank kumar ratre <mayankratre10@gmail.com>
2023-05-31 16:15:29 +0530

    fix details page navigation

--- client/src/components/card/movieCard.jsx
+++ client/src/components/card/movieCard.jsx
@@ -7,7 +7,7 @@ const movieCard = ({details,cardClass}) => {
   const navigate = useNavigate();
   const cardRef=useRef();
   return (
-    <div className={(cardClass ?  cardClass: "card") } ref={cardRef} onClick={()=>navigate('/details/'+details.id+"?tittle=movie")}> 
+    <div className={(cardClass ?  cardClass: "card") } ref={cardRef} onClick={()=>navigate('/UNWIND/details/'+details.id+"?tittle=movie")}> 
       {details && <LazyLoadImage src={import.meta.env.VITE_IMAGE_URL+details.poster_path} className='lazy-load'/>}
       {details && <div className="rating">{details.vote_average.toFixed(1)}</div>}
     </div>
